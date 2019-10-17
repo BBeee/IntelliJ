eb3fd45d895674634f7c8db660b588ad362e141f
BBeee <jeeesong@gmail.com>
2019-10-17 14:25:38 -0400

    removed

--- Python/Alpha Vantage Python/testAL.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=06NCM8Z6D0E5CNQ6")
-dir(response)
-print(response.json())
